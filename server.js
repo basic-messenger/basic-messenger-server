3aedf79ec68601b2ae5dda7e728f3d4527cb336a
antoni <antoni.dev0@gmail.com>
2021-11-10 20:22:06 -0800

    adds basic server logic on server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const app = require('express')
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {cors:{origin: '*'}})
+const PORT = 8000
+
+io.on('connection', socket => {
+console.log(`connected client : ${socket.id}`)
+
+  socket.on('client-chatter', (payload) => {
+    console.log('payload: ',payload)
+    io.emit('client-chatter',payload)
+  })
+
+  socket.on('disconnect', ()=>{
+    console.log(`disconnected client: ${socket.id}`)
+    io.emit('disconect', `${socket.id} has disconected`)
+  })
+
+})
+
+server.listen(PORT, ()=>{
+console.log(`chat server @ PORT: ${PORT}...`)
+})
